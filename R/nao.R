507bd8e27437f40d153ecc43fe8b3a4ce508df5e
Layton <Chantelle.Layton@dfo-mpo.gc.ca>
2022-01-06 11:43:03 -0400

    handle new format

--- R/nao.R
+++ R/nao.R
@@ -26,42 +26,63 @@ download.nao <- function(destdir = '.', destfile = NULL){
     dir.create(destdir, recursive = TRUE)
   }
   # define url to download data
-  url <- 'https://www.ncdc.noaa.gov/teleconnections/nao/data.csv'
+  #url <- 'https://www.ncdc.noaa.gov/teleconnections/nao/data.csv'
+  # late 2021/early 2022 changed, found on 20220106
+  url <- 'https://www.cpc.ncep.noaa.gov/products/precip/CWlink/pna/norm.nao.monthly.b5001.current.ascii.table'
   # check if filename is supplied, if not use the filename from
   # the url
   if(is.null(destfile)){
     destfile <- basename(url)
+    ss <- strsplit(x = destfile, split = '\\.')[[1]]
+    if(ss[length(ss)] != 'csv'){ # save new format as .txt file as it is tab deliminated
+      destfile <- paste(destfile, 'txt', sep = '.')
+    }
   }
-
   download.file(url = url, destfile = paste(destdir, destfile, sep = '/'))
 }
 
 #' @title Read North Atlantic Oscillation Index file
-#' 
+#'
 #' @description This function reads in the north atlantic oscillation index
 #' comma separated file.
-#' 
+#'
 #' @param file a connection or a character string giving the name of the file to load.
-#' 
+#'
 #' @return a data frame with columns named year, month, and value
-#' 
+#'
 #' @author Chantelle Layton
-#' 
+#'
 #' @importFrom utils read.csv
-#' 
+#'
 #' @export
-#' 
+#'
 
 read.nao <- function(file){
-  d <- read.csv(file, skip = 1, header = TRUE)
-  # first column has the header named Date
-  # it has the year and month combined
-  # characters 1-4 are the year, characters 5-6 is the month
-  # extract them
-  year <- as.numeric(substr(d$Date, 1, 4))
-  month <- as.numeric(substr(d$Date, 5, 6))
-  df <- data.frame(year = year,
-                   month = month,
-                   value = d$Value)
-  df
+  fext <- strsplit(basename(file), sep = '\\.')[[1]]
+  if(fext[length(fext)] == 'csv'){ # data format for 2019 to 2021 data
+    d <- read.csv(file, skip = 1, header = TRUE)
+    # first column has the header named Date
+    # it has the year and month combined
+    # characters 1-4 are the year, characters 5-6 is the month
+    # extract them
+    year <- as.numeric(substr(d$Date, 1, 4))
+    month <- as.numeric(substr(d$Date, 5, 6))
+    df <- data.frame(year = year,
+                     month = month,
+                     value = d$Value)
+    df
+  }
+  if(fext[length(fext)] == 'txt'){ # different data format identified 20220116, see download.nao for details
+    d <- read.table(file) # this will be a data.frame
+    # first column is the year
+    # columns after that are the months
+    colnames(d) <- c('Year', month.abb)
+    year <- d[['Year']]
+    month <- rep(1:12, length(year))
+    value <- as.numeric(t(d[, names(d) != 'Year']))
+    df <- data.frame(year = year,
+                     month = month,
+                     value = value)
+    df
+  }
 }
