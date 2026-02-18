796545e5c1319f227427e331815508db56356cee
Chantelle Layton <layton.chantelle@gmail.com>
2026-02-18 10:25:19 -0400

    update for new file format

--- R/sealevel2.R
+++ R/sealevel2.R
@@ -4,7 +4,7 @@
 #' that allows for easy use for calculations and data output. It's similar in nature
 #' to as.ahccd.
 #'
-#' @param file a connection or a character string or vector giving the name(s) of the
+#' @param file a connection or a character string or list giving the name(s) of the
 #' file(s) to load.
 #' @param combineData a logical value indicating whether or not to combine data
 #' into a single time-series if the length of files is greater than one. If the
@@ -13,6 +13,7 @@
 #' @author Chantelle Layton
 #'
 #' @importFrom oce read.sealevel
+#' @importFrom oce read.sealevel.gc2026
 #'
 #' @export
 
@@ -21,29 +22,30 @@ read.sealevel2 <- function(file, combineData = TRUE){
   # put it into format for easy use
   if(length(file) == 1){
     dd <- read.sealevel(file)
-    dl <- list(stationNumber = dd[['stationNumber']],
-               stationName = dd[['stationName']],
-               longitude = dd[['longitude']] * -1, # assuming longitude always needs to be changed to negative
-               latitude = dd[['latitude']],
+    dl <- list(stationNumber = ifelse('stationNumber' %in% names(dd@metadata), dd[['stationNumber']], dd[['Station Code - Code de la station']]),
+               stationName = ifelse('stationName' %in% names(dd@metadata), dd[['stationName']], dd[['Station Name - Nom de la station']]),
+               longitude = ifelse('longitude' %in% names(dd@metadata), dd[['longitude']] * -1, dd[['Longitude - Longitude']]),
+               latitude = ifelse('latitude' %in% names(dd@metadata), dd[['latitude']], dd[['Latitude - Latitude']]),
                data = data.frame(time = dd[['time']],
                                  elevation = dd[['elevation']]
                                  ))
   } else {
-    dd <- lapply(file, read.sealevel)
+    dd <- lapply(file, function(k) if(length(k) == 1) read.sealevel(k) else read.sealevel.gc2026(k))
     if(combineData){
-      dl <- list(stationNumber = dd[[1]][['stationNumber']],
-                 stationName = dd[[1]][['stationName']],
-                 longitude = dd[[1]][['longitude']] * -1, # see above
-                 latitude = dd[[1]][['latitude']],
+      k <- dd[[1]]
+      dl <- list(stationNumber = ifelse('stationNumber' %in% names(k@metadata), k[['stationNumber']], k[['Station Code - Code de la station']]),
+                 stationName = ifelse('stationName' %in% names(k@metadata), k[['stationName']], k[['Station Name - Nom de la station']]),
+                 longitude = ifelse('longitude' %in% names(k@metadata), k[['longitude']] * -1, k[['Longitude - Longitude']]),
+                 latitude = ifelse('latitude' %in% names(k@metadata), k[['latitude']], k[['Latitude - Latitude']]),
                  data = data.frame(time = as.POSIXct(unlist(lapply(dd, function(k) k[['time']])),
                                                      origin = '1970-01-01',
                                                      tz = 'UTC'),
                                    elevation = unlist(lapply(dd, function(k) k[['elevation']]))))
     } else {
-      dl <- lapply(dd, function(k) list(stationNumber = k[['stationNumber']],
-                                        stationName = k[['stationName']],
-                                        longitude = k[['longitude']] * -1,
-                                        latitude = k[['latitude']],
+      dl <- lapply(dd, function(k) list(stationNumber = ifelse('stationNumber' %in% names(k@metadata), k[['stationNumber']], k[['Station Code - Code de la station']]),
+                                        stationName = ifelse('stationName' %in% names(k@metadata), k[['stationName']], k[['Station Name - Nom de la station']]),
+                                        longitude = ifelse('longitude' %in% names(k@metadata), k[['longitude']] * -1, k[['Longitude - Longitude']]),
+                                        latitude = ifelse('latitude' %in% names(k@metadata), k[['latitude']], k[['Latitude - Latitude']]),
                                         data = data.frame(time = as.POSIXct(k[['time']],
                                                                             origin = '1970-01-01',
                                                                             tz = 'UTC'),
